f4a479cd40c4376848068e92dd1cdaa8e5991c16
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-01-31 18:17:56 +0100

    forget-password route wee added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Dashboard from 'pages/Dashboard';
+import ForgetPassword from 'pages/ForgetPassword';
 import Login from 'pages/Login';
 import UsersGeneralDetails from 'pages/UsersGeneralDetails';
 import UsersShowingFilters from 'pages/UsersShowingFilters';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
 
 function App() {
   return (
@@ -14,6 +14,8 @@ function App() {
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/usersFilters' element={<UsersShowingFilters/>}/>
         <Route path='/generalDetails' element={<UsersGeneralDetails/>}/>
+        <Route path='/forget-password' element={<ForgetPassword/>}/>
+
       </Routes>
     </div>
   );
