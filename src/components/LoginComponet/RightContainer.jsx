7b1153137185f460e198399e301595321ef131f8
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-01-31 18:14:17 +0100

    firebase authentication was imported

--- src/components/LoginComponet/RightContainer.jsx
+++ src/components/LoginComponet/RightContainer.jsx
@@ -1,29 +1,53 @@
-import React from "react";
+import {
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useState } from "react";
+import { auth  } from "components/LoginComponet/firebase";
+import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 
+
 const RightContainer = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState("");
+
+  let navigate = useNavigate();
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        setError(<p style={{color:"red"}}>Invalid Email / Password</p>);
+      });
+  };
   return (
     <div className="right-container-wrapper">
       <div className="welcome">
-        <img src="assets/img/Welcome!.png" alt="" />
+        <img src="assets/img/Welcome.png" alt="" />
         <p>Enter details to login</p>
       </div>
-      <form action="">
+      <form onSubmit={signIn}>
+        {error}
         <div className="password-wrapper">
-          <input type="email" placeholder="Email" />
+          <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email" required/>
         </div>
         <div className="password-wrapper">
-          <input type="password" placeholder="Password" />
-          <span className="show-wrapper">show</span>
+          <input type={`${show ? "text" : "password"}`} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+          <span className="show-wrapper" onClick={() => setShow(!show)}>show</span>
         </div>
+        <Link to="/forget-password" style={{textDecoration: "none"}}>
         <div className="forget-password">
           <p>Forget Password</p>
         </div>
-        <Link to="/dashboard">
-        <div className="button-wrapper">
-          <button>LOG IN</button>
-        </div>
         </Link>
+        <div className="button-wrapper">
+          <button type="submit">LOG IN</button>
+        </div>
       </form>
     </div>
   );
