14889f7a78c7e603414adba5af5b2ddab6c8448d
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-01-31 18:16:25 +0100

    props version and  logout  were passed into the sidebar file

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -4,25 +4,30 @@ import Settings from "components/sideBar/Settings";
 import SwitchOrganization from "components/sideBar/SwitchOrganization";
 import Dashboard from "components/sideBar/Dashboard";
 import { Link } from "react-router-dom";
+import { BsBoxArrowRight } from "react-icons/bs";
 
-const SideBar = () => {
+const SideBar = ({ logout, version }) => {
   return (
     <div className="sideBar-wrapper">
       <Link to="/">
-      <div className="brand-logo">
-        <div className="union-logo">
-          <img src="assets/img/Union.png" alt="" />
+        <div className="brand-logo-sidebar">
+          <div className="union-logo">
+            <img src="assets/img/Union.png" alt="" />
+          </div>
+          <div className="lendsqr-logo">
+            <img src="assets/img/lendsqr.png" alt="" />
+          </div>
         </div>
-        <div className="lendsqr-logo">
-          <img src="assets/img/lendsqr.png" alt="" />
-        </div>
-      </div>
       </Link>
-      <SwitchOrganization/>
-      <Dashboard/>
+      <SwitchOrganization />
+      <Dashboard />
       <Customers />
       <Businesses />
       <Settings />
+      <div className="logout-version">
+        <div className="logout"><BsBoxArrowRight style={{marginRight: "10px"}}/>{logout}</div>
+        <div className="version">{version}</div>
+      </div>
     </div>
   );
 };
