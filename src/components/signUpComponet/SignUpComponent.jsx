980c57f9ad553ab7629b81dc812135be965cf755
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-02-01 07:40:29 +0100

    signup components was created

---
+++ src/components/signUpComponet/SignUpComponent.jsx
@@ -0,0 +1,15 @@
+import 'components/signUpComponet/stylesSignUp.scss';
+import LeftContainer from 'components/signUpComponet/LeftContainer'
+import RightContainer from 'components/signUpComponet/RightContainer';
+
+const SignUpComponent = () => {
+  return (
+    <div className='Login-component'>
+        <LeftContainer/>
+        <RightContainer/>
+    </div>
+
+  )
+}
+
+export default SignUpComponent;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import SignUpComponent from "components/signUpComponet/SignUpComponent";
+
+
+const SignUp = () => {
+  return (
+    <div>
+      <SignUpComponent/>
+    </div>
+  )
+}
+
+export default SignUp;
