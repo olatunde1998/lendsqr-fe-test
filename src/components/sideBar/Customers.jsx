e14de66a036cf8f9baddbd9247e7eaf1df939621
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-02-04 12:35:07 +0100

    customers components was formatted

--- src/components/sideBar/Customers.jsx
+++ src/components/sideBar/Customers.jsx
@@ -1,55 +1,120 @@
 import { MdLockOutline } from "react-icons/md";
-import { FaHandHoldingUsd, FaHandshake, FaUserCheck, FaUserFriends, FaUsers, FaUserTimes } from "react-icons/fa";
+import {
+  FaHandHoldingUsd,
+  FaHandshake,
+  FaUserCheck,
+  FaUserFriends,
+  FaUsers,
+  FaUserTimes,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Customers = () => {
-    
   return (
     <div>
-        <p className='customer-headings'>Customers</p>
-        <Link to="/generalDetails" style={{textDecoration:"none", cursor:"pointer"}}>
-        <div className='customers-box'>
-            <FaUserFriends/>
-            <p className='icon-name icon-name-1'>Users</p>
+      <p className="customer-headings">Customers</p>
+      <Link
+        to="/users"
+        style={{ textDecoration: "none", cursor: "pointer" }}
+      >
+        <div className="customers-box customer-box-1">
+          <FaUserFriends />
+          <p className="icon-name icon-name-1">Users</p>
         </div>
+      </Link>
+      <div className="customers-box">
+        <FaUsers />
+        <Link
+          to="/guarantors"
+          style={{
+            textDecoration: "none",
+            color: "#8496bb",
+            cursor: "pointer",
+          }}
+        >
+          <p className="icon-name">Guarantors</p>
         </Link>
-        <div  className="customers-box">
-            <FaUsers/>
-            <Link to="/guarantors" style={{textDecoration: "none", color: "#8496bb", cursor: "pointer"}}><p className='icon-name'>Guarantors</p></Link>
-        </div>
-        <div className='customers-box'>
-            <MdLockOutline/>
-            <Link to="/loans" style={{textDecoration: "none", color: "#8496bb", cursor: "pointer"}}>
-            <p className='icon-name'>Loans</p></Link>
-        </div>
-        <div className='customers-box'>
-            <FaHandshake/>
-            <Link to="/decision-models" style={{textDecoration: "none", color: "#8496bb", cursor: "pointer"}}>
-            <p className='icon-name'>Decision Models</p></Link>
-        </div>
-        <div className='customers-box'>
-            <MdLockOutline/>
-            <Link to="/savings" style={{textDecoration: "none", color: "#8496bb", cursor: "pointer"}}>
-            <p className='icon-name'>Savings</p></Link>
-        </div>
-        <div className='customers-box'>
-            <FaHandHoldingUsd/>
-            <Link to="/loan-requests" style={{textDecoration: "none", color: "#8496bb", cursor: "pointer"}}>
-            <p className='icon-name'>Loan Requests</p></Link>
-        </div>
-        <div className='customers-box'>
-            <FaUserCheck/>
-            <Link to="/whitelist" style={{textDecoration: "none", color: "#8496bb", cursor: "pointer"}}>
-            <p className='icon-name'>Whitelist</p></Link>
-        </div>
-        <div className='customers-box'>
-            <FaUserTimes/>
-            <Link to="/loans" style={{textDecoration: "none", color: "#8496bb", cursor: "pointer"}}>
-            <p className='icon-name'>Karma</p></Link>
-        </div>
-
+      </div>
+      <div className="customers-box">
+        <MdLockOutline />
+        <Link
+          to="/loans"
+          style={{
+            textDecoration: "none",
+            color: "#8496bb",
+            cursor: "pointer",
+          }}
+        >
+          <p className="icon-name">Loans</p>
+        </Link>
+      </div>
+      <div className="customers-box">
+        <FaHandshake />
+        <Link
+          to="/decision-models"
+          style={{
+            textDecoration: "none",
+            color: "#8496bb",
+            cursor: "pointer",
+          }}
+        >
+          <p className="icon-name">Decision Models</p>
+        </Link>
+      </div>
+      <div className="customers-box">
+        <MdLockOutline />
+        <Link
+          to="/savings"
+          style={{
+            textDecoration: "none",
+            color: "#8496bb",
+            cursor: "pointer",
+          }}
+        >
+          <p className="icon-name">Savings</p>
+        </Link>
+      </div>
+      <div className="customers-box">
+        <FaHandHoldingUsd />
+        <Link
+          to="/loan-requests"
+          style={{
+            textDecoration: "none",
+            color: "#8496bb",
+            cursor: "pointer",
+          }}
+        >
+          <p className="icon-name">Loan Requests</p>
+        </Link>
+      </div>
+      <div className="customers-box">
+        <FaUserCheck />
+        <Link
+          to="/whitelist"
+          style={{
+            textDecoration: "none",
+            color: "#8496bb",
+            cursor: "pointer",
+          }}
+        >
+          <p className="icon-name">Whitelist</p>
+        </Link>
+      </div>
+      <div className="customers-box">
+        <FaUserTimes />
+        <Link
+          to="/loans"
+          style={{
+            textDecoration: "none",
+            color: "#8496bb",
+            cursor: "pointer",
+          }}
+        >
+          <p className="icon-name">Karma</p>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Customers;
